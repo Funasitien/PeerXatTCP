19bd6b315cbb05c96eea99274f111bade1e02bd8
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2023-09-15 12:58:05 +0200

    ⚡️ Code mis en fonction

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+# PeerXat Launcher. Info @ peerxat.eu.org 
+
 from config import *
 
-a = input("Voulez vous lancer le client ou le serveur ? [S/C]")
-if a == "S" or a == "Serveur":
-    print("Serveur")
-else:
-    from client import TCPClient
-    TCPClient.launch(IP, PORT)
+def peerxat():
+    a = input("Voulez vous lancer le client ou le serveur ? [S/C] ")
+    if a == "S" or a == "Serveur":
+        from server import TCPServer
+        name = input("Comment voulez vous appeler votre serveur [exit pour quitter] ")
     
+        if name != "exit":
+        
+            server = TCPServer("Test")
+            server.run()
+        
+        else:
+            peerxat()
+    else:
+        from client import TCPClient
+        TCPClient.launch(IP, PORT)
+
+peerxat()
